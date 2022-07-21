7277388bb233f4f14bef50be4a687992c09640fe
jeanjacquesserpoul <serpoul@gmail.com>
2022-07-21 05:06:28 +0200

    Remove global variables

--- dealgen/generate.go
+++ dealgen/generate.go
@@ -61,7 +61,11 @@ func delta(slice []int, ToRemove []int) []int {
 }
 
 func getInitDeal() []int {
-	return initDeal
+	return []int{
+		0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
+		17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35,
+		36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51,
+	}
 }
 
 func MaskToArray(pbn string) ([]int, []int) {
@@ -212,10 +216,14 @@ func handPbn(h []int) string {
 	return r
 }
 
+func getValueCards(i int) int {
+	var valueCards = []int{0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4}
+	return valueCards[i]
+}
 func pointsFromHand(h []int) int {
 	v := 0
 	for _, value := range h {
-		v += valueCards[cardValueInt(value)]
+		v += getValueCards(cardValueInt(value))
 	}
 	return v
 }
